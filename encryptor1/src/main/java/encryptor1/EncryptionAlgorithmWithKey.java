737bf201f248cc0e93f51777a53e8d3b660cde34
almoggal <almog.6@hotmail.com>
2016-02-28 22:19:22 +0200

    Update EncryptionAlgorithmWithKey.java

--- encryptor1/src/main/java/encryptor1/EncryptionAlgorithmWithKey.java
+++ encryptor1/src/main/java/encryptor1/EncryptionAlgorithmWithKey.java
@@ -9,14 +9,10 @@ public abstract class EncryptionAlgorithmWithKey implements EncryptionAlgorithm<
 	 * but with a given key.
 	 * Checks if the key is out of bounds, if it is throws a corresponding exception.
 	 */
-	public EncryptionAlgorithmWithKey(int key) {
+	public EncryptionAlgorithmWithKey(int key) throws InvalidEncryptionKeyException{
 		if(!(key >=0 && key<256)){
-			try {
-				throw new InvalidEncryptionKeyException();
-			} catch (InvalidEncryptionKeyException e) {
+			throw new InvalidEncryptionKeyException();
 				
-				e.printStackTrace();
-			}
 		}else this.key = key;
 	}
 	
